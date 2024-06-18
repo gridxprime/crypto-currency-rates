db6c6338d088d70c24261291fd467599cb5ab07d
tillmorningcomes <ender.kraya@gmail.com>
2024-06-18 09:44:47 +0500

    v1.3 'crypto pages added 2/6'

--- src/components/pages/Btc.jsx
+++ src/components/pages/Btc.jsx
@@ -6,13 +6,13 @@ import btcIcon from '../../assets/img/Main/btc.svg'
 
 function Btc() {
 
-  const [btcPrice, setBtcPrice] = useState(0)
+  const [cryptoPrice, setCryptoPrice] = useState(0)
 
   const fetchCryptoPrice = async() => {
     try {
-      const btcResponse = await fetch('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT');
-      const btcData = await btcResponse.json();
-      setBtcPrice(btcData.price);
+      const cryptoResponse = await fetch('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT');
+      const cryptoData = await cryptoResponse.json();
+      setCryptoPrice(cryptoData.price);
     } catch (error) {
       console.error('Error fetching cryptocurrency prices:', error);
     }
@@ -38,7 +38,7 @@ function Btc() {
     <div className="crypto__rate">
 
     <span className='crypto__price'>
-      {btcPrice}
+      {cryptoPrice}
     </span>
     <span className='crypto__desc'>
     Bitcoin - это криптовалюта, созданная в 2009 году
--- src/components/pages/Eth.jsx
+++ src/components/pages/Eth.jsx
@@ -1,10 +1,67 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './style.css'
+import './media.css'
+import Footer from '../Footer.jsx'
+import ethIcon from '../../assets/img/Main/eth.svg'
+
 function Eth() {
+
+  const [cryptoPrice, setCryptoPrice] = useState(0)
+
+  const fetchCryptoPrice = async() => {
+    try {
+      const cryptoResponse = await fetch('https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT');
+      const cryptoData = await cryptoResponse.json();
+      setCryptoPrice(cryptoData.price);
+    } catch (error) {
+      console.error('Error fetching cryptocurrency prices:', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchCryptoPrice();
+
+    const interval = setInterval(fetchCryptoPrice, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
+  
+  <>
+
 <section className='rates__page'>
-dasfsdafasd
+    <div className="crypto__inner">
+      <h2> Курс Ethereum</h2>
+      <img style={{width: '50px'}} src={ethIcon} alt="" />
+      </div>
+    <div className="crypto__rate">
+
+    <span className='crypto__price'>
+      {cryptoPrice}
+    </span>
+    <span className='crypto__desc'>
+    Ethereum (ETH) - это вторая по популярности и 
+    капитализации криптовалюта после Bitcoin. Запущенная в 
+    2015 году Виталиком Бутериным, Ethereum представляет собой
+     децентрализованную платформу, которая позволяет разработчикам создавать и запускать 
+    смарт-контракты и децентрализованные приложения (dApps).<br>
+    </br><br></br>
+    Ethereum отличается своей гибкостью и широкими
+     возможностями применения, что привлекает разработчиков 
+     и инвесторов со всего мира. Платформа также поддерживает
+      выпуск новых криптовалют, известных как токены, что позволяет 
+    создавать проекты на базе блокчейна Ethereum.
+
+    </span>
+    </div>
+ 
+
 </section>
+<Footer>
+
+</Footer>
+</>
   )
 }
 
